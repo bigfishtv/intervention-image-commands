f5fcfe85067f98c8665017812a6a492e16b946df
Jacob Clarke <jacob@bigfish.tv>
2016-02-02 18:24:07 +1000

    Tweaked Saturation command to closer match Caman

--- src/CamanSaturationCommand.php
+++ src/CamanSaturationCommand.php
@@ -7,6 +7,12 @@ class CamanSaturationCommand extends \Intervention\Image\Commands\AbstractComman
 	public function execute($image)
     {	
         $saturation = $this->argument(0)->value();
-        return $image->getCore()->modulateImage(100, 100+$saturation, 100);
+        $image->getCore()->modulateImage(100, 100+$saturation, 100);
+        $image->camanCurves([
+			[$saturation/5,	0],
+			[140, 	120],
+			[255,	255]
+		]);
+        return $image;
     }
 }
