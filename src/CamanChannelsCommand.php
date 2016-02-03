b4eb478d8ba365b1ac5b60a218b5d95ba9f668d7
Jacob Clarke <jacob@bigfish.tv>
2016-02-03 20:05:34 +1000

    Channels (tint) uses color matrix now

--- src/CamanChannelsCommand.php
+++ src/CamanChannelsCommand.php
@@ -10,84 +10,27 @@ class CamanChannelsCommand extends \Intervention\Image\Commands\AbstractCommand
     public function execute($image)
     {	
 
-		// modeled to match CamanJS vibrance
-		// for color matrices refer to https://docs.rainmeter.net/tips/colormatrix-guide/
-		/*
-		var hasProp = {}.hasOwnProperty;
-		Filter.register("channels", function(options) {
-		  var chan, value;
-		  for (chan in options) {
-		    if (!hasProp.call(options, chan)) continue;
-		    value = options[chan];
-		    if (value === 0) {
-		      delete options[chan];
-		      continue;
-		    }
-		    options[chan] /= 100;
-		  }
-		  if (options.length === 0) {
-		    return this;
-		  }
-		  return this.process("channels", function(rgba) {
-		    if (options.red != null) {
-		      if (options.red > 0) {
-		        rgba.r += (255 - rgba.r) * options.red;
-		      } else {
-		        rgba.r -= rgba.r * Math.abs(options.red);
-		      }
-		    }
-		    if (options.green != null) {
-		      if (options.green > 0) {
-		        rgba.g += (255 - rgba.g) * options.green;
-		      } else {
-		        rgba.g -= rgba.g * Math.abs(options.green);
-		      }
-		    }
-		    if (options.blue != null) {
-		      if (options.blue > 0) {
-		        rgba.b += (255 - rgba.b) * options.blue;
-		      } else {
-		        rgba.b -= rgba.b * Math.abs(options.blue);
-		      }
-		    }
-		    return rgba;
-		  });
-		});
-		*/
-
         $img = $image->getCore();
         $channels = $this->argument(0)->value();
         foreach($channels as $key => &$value) {
-        	$value = $value/150;
+        	$value = $value/100;
         }
 
         $r = isset($channels['red']) ? $channels['red'] : 0;
         $g = isset($channels['green']) ? $channels['green'] : 0;
         $b = isset($channels['blue']) ? $channels['blue'] : 0;
 
-        $quantum = $img->getQuantumRange()['quantumRangeLong'];
-        
-        // i do this to closer match caman's pixel editing
-        // caman tends to tint the blacks more so i cut them out in order for the color leveling to work better
-        $image->camanCurves([[0, 25], [127, 120], [255, 255]]); 
-
-        $img->levelImage((0)*$quantum, 1+($r*1.5), (1-$r)*$quantum, Imagick::CHANNEL_RED);
-        $img->levelImage((0)*$quantum, 1+($g*1.5), (1-$g)*$quantum, Imagick::CHANNEL_GREEN);
-        $img->levelImage((0)*$quantum, 1+($b*1.5), (1-$b)*$quantum, Imagick::CHANNEL_BLUE);
-
-        /*
-
-        // attempted using color matrix to slightly different effect
-		$img->setColorspace(Imagick::COLORSPACE_RGB);
 		$img->recolorImage([
-			1,		0,		0,		0,		0,
-			0,		1,		0,		0,		0,
-			0,		0,		1,		0,		0,
+			1,		0,		0,		$r,		0,
+			0,		1,		0,		$g,		0,
+			0,		0,		1,		$b,		0,
 			0,		0,		0,		1,		0,
 			0,		0,		0,		0,		0,
 		]);
-
+		return $img;
         
+        /*
+
         // this matches caman's exact pixel filter but takes a ridiculous amount of time to render
         $iterator = $img->getPixelIterator();
         foreach ($iterator as $row => $pixels) {
